df573e2b9c44c88d36083a21356c8ff97768386f
Ryo <21A3039@sit.shonan-it.ac.jp>
2022-05-17 15:10:09 +0900

    1st commit

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-from http.client import PROXY_AUTHENTICATION_REQUIRED
 import streamlit as st
-import numpy as np
-import pandas as pd
-from sympy import python
-from PIL import Image
 import time
 
 st.title('Streamlit 超入門')
